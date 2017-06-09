fa15445a2875cb18fb6e0e886e42fb1f6ec91d84
itai257 <itai257@gmail.com>
2017-06-09 15:08:40 +0300

    Secretary add

--- G9HAS/src/controller/SecretaryController.java
+++ G9HAS/src/controller/SecretaryController.java
@@ -1,5 +1,7 @@
 package controller;
 
+
+import java.awt.List;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -7,6 +9,8 @@ import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -15,9 +19,16 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.Pagination;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
@@ -28,8 +39,63 @@ public class SecretaryController implements Initializable{
 
 	@FXML
 	private Label lblUser;
-	 
 	
+
+	
+	
+	    @FXML
+	    private ComboBox<?> cmbChooseCourse;
+	    
+	    private ComboBox<String> cmbTeacher;
+
+	    @FXML
+	    private ComboBox<?> cmbYear;
+
+	    @FXML
+	    private ListView<String> lvClasses;
+	    
+	    @FXML
+	    private TableView<String> tblClassTeacher;
+	    
+	    @FXML
+	    private TableView<String> tblExceptions;
+	    
+	    @FXML
+	    private Button btnAssign;
+	   
+	    
+
+	    @FXML
+	    private TableColumn<String,String> teachers;
+	    
+	    @FXML
+	    private TableColumn<String,String> classes;
+
+	    @FXML
+	    void listViewEditChoise(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void classClickHandler(ActionEvent event) {
+
+	    }
+	    
+
+	    @FXML
+	    void chooseCourseHandler(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void chooseYearHandler(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void chooseSemesterHandler(ActionEvent event) {
+
+	    }
 	@FXML
 	void logoutHandler(ActionEvent event) {//goes back to login window
 		Parent nextWindow;
@@ -89,6 +155,21 @@ public class SecretaryController implements Initializable{
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		lblUser.setText(UserClient.fullName);
+	        lvClasses.getItems().addAll("a","b","c","d","e","f");
+	        lvClasses.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+	        cmbTeacher = new ComboBox<>();
+	        //TableColumn<String,ComboBox<String>> column = new TableColumn<>();
+	        ObservableList <String> a = FXCollections.observableArrayList();
+	        for(int i=0;i<10;i++)
+	        	a.add("abs");
+	        
+	        tblClassTeacher.setItems(a);
+	        
+
+	    
+	
+	
+	        
 	}
 }
 
